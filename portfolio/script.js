677ffa643e6c131ef0bf7886246566104073f275
Jefferston <dima.dima.989@gmail.com>
2018-05-10 17:54:12 +0300

    tc

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,14 @@
 document.querySelector('.but a').onclick = ev => ev.preventDefault();
+
+let leftBlock = document.querySelector('.leftBlock');
+let rightBlock = document.querySelector('.rightBlock');
+let openLeftBlock = document.querySelector('.openLeft');
+let openRightBlock = document.querySelector('.openRight');
+
+openLeftBlock.addEventListener('click', ev => {
+    leftBlock.classList.toggle('clickedLeft');
+});
+
+openRightBlock.addEventListener('click', ev => {
+    rightBlock.classList.toggle('clickedRight');
+})
