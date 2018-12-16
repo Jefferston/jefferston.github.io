ed9d33a643df973a2da5216d5599b5d26a950ccb
Jefferston <dima.dima.989@gmail.com>
2018-12-17 00:18:57 +0200

    readded

---
+++ adventure/precache-manifest.ddb2935ce9ea10a54ec0f535083d729e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8947ef05bccbccb126e075e71f5afa92",
+    "url": "/static/media/fon.8947ef05.jpg"
+  },
+  {
+    "revision": "965ed122dfa2339b2aa59688aa75d39a",
+    "url": "/static/media/finn.965ed122.png"
+  },
+  {
+    "revision": "337a4119783dcab36745ddf4aa9a419d",
+    "url": "/static/media/buble.337a4119.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e611982d0332ea057db0",
+    "url": "/static/js/main.e611982d.chunk.js"
+  },
+  {
+    "revision": "038d149cf2bb22d5b70c",
+    "url": "/static/js/1.038d149c.chunk.js"
+  },
+  {
+    "revision": "e611982d0332ea057db0",
+    "url": "/static/css/main.4b5027e4.chunk.css"
+  },
+  {
+    "revision": "b23512f378bfd5cdc9cf5ac5357b8ed7",
+    "url": "/index.html"
+  }
+];
