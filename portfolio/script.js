8eec0bd6c6c08f340242a04922aa7526bf7c7457
Jefferston <dima.dima.989@gmail.com>
2018-12-20 23:05:19 +0200

    script

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,17 +1,26 @@
 let descs = document.querySelectorAll('.description');
 let closeButs = document.querySelectorAll('.descBlock button');
+let wrappers = document.querySelectorAll('.projectPage .container .wrapper');
 
 for (let desc of descs) {
-    desc.addEventListener('click', (e) => {
-        e.preventDefault();
-    
-        desc.nextElementSibling.style.visibility = 'visible';
-    });
+    for (let wrap of wrappers) {
+        desc.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            wrap.style.perspective = 'none';
+            desc.nextElementSibling.style.visibility = 'visible';
+        });
+    }
 }
 
 for (let closeBut of closeButs) {
-    closeBut.addEventListener('click', (e) => {
+    for (let wrap of wrappers) {
+        closeBut.addEventListener('click', (e) => {
 
-        closeBut.parentElement.style.visibility = 'hidden';
-    });
+            wrap.style.perspective = '500px';
+            closeBut.parentElement.style.visibility = 'hidden';
+        });
+    }
 }
+
+document.querySelector('.projectPage h1').textContent = '< My Projects />'
