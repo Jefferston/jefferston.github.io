2b6f1a6dcee6362f1764ed91fdf85a29aef660ad
Jefferston <dima.dima.989@gmail.com>
2019-04-26 21:03:48 +0300

    O hope final

--- grund/src/components/Skills.jsx
+++ grund/src/components/Skills.jsx
@@ -3,13 +3,67 @@ import React from 'react';
 export default function Skills() {
     return (
         <div className="skills">
+
+            <h1>Skills & Perks</h1>
+
             <div className="skills-description">
-                <h1>Skills & Perks</h1>
-                <p><span>[HTML, CSS]:</span> Less/Sass, Styled Components, Twig</p>
-                <p><span>[Javascript]:</span> ES6/ES7, JQuery, Webpack, Gulp</p>
-                <p><span>[React]:</span> React-routing, Redux</p>
-                <p><span>[Backend]:</span> Node.js, Express, Axios, MongoDB</p>
-                <p><span>[Others]:</span> TypeScript</p>
+
+                <div className="html-css">           
+                    <h2>HTML / CSS</h2>
+
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    
+                </div>
+
+                <div className="js">
+
+                    <h2>Javascript</h2>
+
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                </div>
+
+                <div className="react">
+
+                    <h2>React</h2>
+
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                </div>
+
+                <div className="backend">
+
+                    <h2>Backend</h2>
+
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                </div>
+
+                <div className="other">
+
+                    <h2>Others</h2>
+
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+                
             </div>
         </div>
     )
