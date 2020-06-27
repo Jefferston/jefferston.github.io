825cfde482a661ee9cb4bc186f45bdb4948f2ba0
Jefferston <dima.dima.989@gmail.com>
2020-06-27 22:38:23 +0200

    engineer

--- blogProj/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: 3000,
-    MONGO_URL: 'mongodb://localhost/blog',
-    IS_PRODUCTION: true,
-    SESSION_SECRET: 'sgjios14kwgjr12iol',
-    PER_PAGE: 3,
-    DESTINATION: 'uploads'
-}
