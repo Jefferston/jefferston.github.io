528a0ae2f74b05fc3a76aa9ecfc29d40c7299b1a
Jefferston <dima.dima.989@gmail.com>
2020-09-01 13:08:56 +0200

    a lot

--- engineer/script.js
+++ engineer/script.js
@@ -2,6 +2,7 @@ let burger = document.querySelector('.leftSideBurger');
 let ava = document.querySelector('.leftSide');
 let text = document.querySelector('.rightSide');
 let darkness = document.querySelector('.contentDarkness');
+let body = document.querySelector('body');
 
 burger.onclick = () => {
     if (window.getComputedStyle(darkness).display == 'none') {
@@ -9,6 +10,7 @@ burger.onclick = () => {
         ava.style.backgroundColor = '#333';
         ava.style.backgroundImage = 'none';
         text.style.overflowY = 'hidden';
+        body.style.overflowY = 'hidden';
         burger.style.borderRadius = '100%';
         burger.textContent = 'X';
         burger.style.backgroundImage = 'none';
@@ -16,6 +18,7 @@ burger.onclick = () => {
         darkness.style.display = 'none';
         ava.style.backgroundImage = "url('./me.jpg')";
         text.style.overflowY = 'auto';
+        body.style.overflowY = 'auto';
         burger.style.borderRadius = '0';
         burger.textContent = '';
         burger.style.backgroundImage = "url('./menu.png')";
