8873a029249ffd6dcc7274ef72c65996e7893346
Jefferston <dima.dima.989@gmail.com>
2022-02-18 20:34:08 +0100

    good enough

--- portfolio/js.js
+++ portfolio/js.js
@@ -1,7 +1,7 @@
 let iconArray = document.querySelectorAll(".iconUnit");
 let windowArray = document.querySelectorAll(".window");
 let btnArray = document.querySelectorAll(".window .controls .closeBtn");
-
+let postArray = document.querySelectorAll(".postContainer .post");
 
 for (let icon of iconArray) {
 
@@ -14,7 +14,6 @@ for (let icon of iconArray) {
 
     icon.addEventListener('dblclick', (el) => {
         let text = el.currentTarget.querySelector('.icon_text');
-        console.log(text.textContent)
         for (let window of windowArray) {
             if (window.getAttribute('data-name') == text.textContent) {
                 window.style.display = 'block';
@@ -23,7 +22,24 @@ for (let icon of iconArray) {
     })
 }
 
-
-for (btn of btnArray) {
+for (let btn of btnArray) {
     btn.onclick = (el) => el.target.parentElement.parentElement.style.display = 'none';
 }
+
+for (let post of postArray) {
+    post.addEventListener('click', (el) => {
+
+        let url = getComputedStyle(el.target).getPropertyValue('background-image')
+        document.querySelector('.postBig').style.display = 'block';
+        document.querySelector('.postBig').style.backgroundImage = url
+        // console.log(getComputedStyle(el.target).getPropertyValue('background-image'))
+
+        // let bigPic = document.createElement('div');
+        // document.querySelector('.desk').appendChild(bigPic);
+        // bigPic.classList.add('postBig');
+        // bigPic.style.backgroundImage = "url(./img/insta/china.jpg)"
+        // console.log(bigPic)
+    })
+}
+
+document.querySelector('.postBig').onclick = (el) => el.target.style.display = 'none';
