a0e16b43f44a2e040a4c53a4fcf45a25b218bb22
Jefferston <dima.dima.989@gmail.com>
2023-12-22 16:06:30 +0100

    year summary

--- reactapp/src/components/WinRPG.jsx
+++ reactapp/src/components/WinRPG.jsx
@@ -42,13 +42,13 @@ const WinRPG = () => {
 
                   <StandartBlock name="Job experience" type="JobExp">
                     <StandartBlockItem name="Data analyst" value="400"></StandartBlockItem>
-                    <StandartBlockItem name="Project / Product Manager" value="1500"></StandartBlockItem>
+                    <StandartBlockItem name="Project / Product Manager" value="1900"></StandartBlockItem>
                   </StandartBlock>
 
                   <StandartBlock name="Other skills" type="Other">
                     <StandartBlockItem name="Cybersport" value="2500"></StandartBlockItem>
-                    <StandartBlockItem name="Driving" value="600"></StandartBlockItem>
-                    <StandartBlockItem name="World comprehension" value="5000"></StandartBlockItem>
+                    <StandartBlockItem name="Driving" value="1000"></StandartBlockItem>
+                    <StandartBlockItem name="World comprehension" value="5100"></StandartBlockItem>
                     <StandartBlockItem name="Fencing / Swords" value="0"></StandartBlockItem>
                   </StandartBlock>
 
@@ -56,17 +56,17 @@ const WinRPG = () => {
                     <div className="countBlockContainer">
                       <CountBlock name="Bachelor's degree" value="10000"></CountBlock>
                       <CountBlock name="Living abroad" value="5000"></CountBlock>
-                      <CountBlock name="Fluency in 2 foreign languages" value="10000"></CountBlock>
+                      <CountBlock name="Fluency* in 2 foreign languages" value="10000"></CountBlock>
                       <CountBlock name="Perfect storm 2020" value="2000"></CountBlock>
                       <CountBlock name="Developed a blog" value="500"></CountBlock>
-                      <CountBlock name="Money earned" value="500"></CountBlock>
+                      <CountBlock name="Money earned" value="600"></CountBlock>
                       <CountBlock name="Career block" value="0"></CountBlock>
                     </div>
                   </StandartBlock>
 
                   <StandartBlock name="Art" type="Art">
                     <div className="countBlockContainer">
-                      <CountBlock name="Written books" value="1700"></CountBlock>
+                      <CountBlock name="Written books" value="1800"></CountBlock>
                       <CountBlock name="Personal site" value="100"></CountBlock>
                     </div>
                   </StandartBlock>
